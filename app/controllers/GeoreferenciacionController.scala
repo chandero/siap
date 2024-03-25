2718d6ea8c1c696d13c509d871e2545cff418074
Geovanny-A <geovany.afanador@gmail.com>
2024-03-25 14:27:34 -0500

    rutas_by_widget

--- app/controllers/GeoreferenciacionController.scala
+++ app/controllers/GeoreferenciacionController.scala
@@ -13,7 +13,6 @@ import play.api.mvc.AbstractController
 import utilities.ImplicitJsonFormats
 import utilities.DateTimeSerializer
 
-
 import net.liftweb.json._
 import net.liftweb.json.Serialization.write
 import net.liftweb.json.Serialization.read
@@ -28,20 +27,58 @@ class GeoreferenciacionController @Inject()(
     config: Configuration,
     authenticatedUserAction: AuthenticatedUserAction
 )(implicit ec: ExecutionContext)
-    extends AbstractController(cc) with ImplicitJsonFormats {
+    extends AbstractController(cc)
+    with ImplicitJsonFormats {
   implicit val formats = Serialization.formats(NoTypeHints) ++ List(
     DateTimeSerializer
   )
 
-  def getGeoreferencia(t: Integer, barr_id: Int,aap_tecnologia: String ,aap_potencia: Int, aap_id:Int) = authenticatedUserAction.async {
-    implicit request: Request[AnyContent] =>
-      gService.getGeoreferencia(barr_id,aap_tecnologia,aap_potencia,aap_id).map { georeferencia =>
+  def getGeoreferencia(
+      t: Integer,
+      barr_id: Int,
+      aap_tecnologia: String,
+      aap_potencia: Int,
+      aap_id: Int
+  ) = authenticatedUserAction.async { implicit request: Request[AnyContent] =>
+    gService
+      .getGeoreferencia(barr_id, aap_tecnologia, aap_potencia, aap_id)
+      .map { georeferencia =>
         Ok(write(georeferencia))
       }
   }
 
-  def getKey() = authenticatedUserAction  {
+  def getKey() = authenticatedUserAction {
     val apiKey: String = config.get[String]("google.apiKey")
     Ok(apiKey)
-  }  
+  }
+
+  def getGeoreferenciaWeb(
+      t: Integer,
+      barr_id: Int,
+      aap_tecnologia: String,
+      aap_potencia: Int,
+      aap_id: Int,
+      token: String
+  ) = { implicit request: Request[AnyContent] =>
+    val secret = config.get[String]("play.http.secret.key")
+    if (secret == token) {
+      gService
+        .getGeoreferencia(barr_id, aap_tecnologia, aap_potencia, aap_id)
+        .map { georeferencia =>
+          Ok(write(georeferencia))
+        }
+    } else {
+      Ok(write("Token invalido"))
+    }
+  }
+
+  def getKeyWeb(token: String) = {
+    val secret = config.get[String]("play.http.secret.key")
+    if (secret == token) {
+      val apiKey: String = config.get[String]("google.apiKey")
+      Ok(apiKey)
+    } else {
+      Ok(write("Token invalido"))
+    }
+  }
 }
